da776cecef6a785b530f41d0e484e6283b6345b2
박래인 <parkrain1189@gmail.com>
2023-05-13 23:59:09 +0900

    Fix theme return to light when page reloads

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-function toNight() {
+if (document.cookie.includes('dark')) {
+    toDark()
+}
+
+function toDark() {
     document.querySelector('body').style.backgroundColor="black";
     document.querySelector('body').style.color="lightgrey";
     ats = document.querySelectorAll('a');
     ats.forEach(function(a) {
         a.style.color="lightblue"
     })
+    document.cookie = "theme=dark"
 }
 
-function toDay() {
+function toLight() {
     document.querySelector('body').style.backgroundColor=null;
     document.querySelector('body').style.color=null;
     ats = document.querySelectorAll('a');
     ats.forEach(function(a) {
         a.style.color=null
     })
+    document.cookie = "theme=light"
 }
+
+// function getCookies() {
+//     const cookieString = document.cookie;
+//     const cookieList = cookieString.split(';');
+//     const cookies = {};
+
+//     for (let i = 0; i += 1; i < cookieList.length) {
+//         let name = cookieList[0].split('=')[0];
+//         let value = cookieList[0].split('=')[1];
+//         cookies[name] = value;
+//     }
+// }
