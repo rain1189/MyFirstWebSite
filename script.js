389d94e10c7d4e5ca7c1d3f8714e3789525b7b9a
박래인 <parkrain1189@gmail.com>
2023-05-13 20:01:34 +0900

    change theme position

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toNight() {
     document.querySelector('body').style.backgroundColor="black";
     document.querySelector('body').style.color="lightgrey";
-    a들 = document.querySelectorAll('a');
-    a들.forEach(function(a) {
+    as = document.querySelectorAll('a');
+    as.forEach(function(a) {
         a.style.color="lightblue"
     })
 }
@@ -10,8 +10,8 @@ function toNight() {
 function toDay() {
     document.querySelector('body').style.backgroundColor=null;
     document.querySelector('body').style.color=null;
-    a들 = document.querySelectorAll('a');
-    a들.forEach(function(a) {
+    as = document.querySelectorAll('a');
+    as.forEach(function(a) {
         a.style.color=null
     })
 }
