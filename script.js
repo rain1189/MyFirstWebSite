60af0b2e22dda42c0e7c53dfc45006803c57dbcc
박래인 <parkrain1189@gmail.com>
2023-05-15 00:12:42 +0900

    Add date and image surprise

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const body = document.querySelector('body');
 const togle = document.querySelector('#togle');
+const date = document.querySelector('#date');
+const image = document.querySelector('img');
 
 if (document.cookie.includes("dark")) {
     toDark();
 }
+
+image.addEventListener('click', handleImageClick)
+
+setInterval(function() {
+    date.innerText = Date().slice(0,24);
+}, 1000);
 
 function themeChange() {
     if (togle.value === "dark") {
@@ -25,6 +33,26 @@ function toLight() {
     body.style.color=null;
     togle.value="dark";    
     document.cookie="theme=light";
+}
+
+function handleImageClick() {
+    let enlargeCount = 1;
+    image.height *= 2;
+    const enlargeImage = setInterval(() => {
+        image.height *= 2;
+        enlargeCount++;
+        if (enlargeCount === 6) {
+            clearInterval(enlargeImage);
+            let reduceCount = 0;
+            const reduceImage = setInterval(() => {
+                image.height /= 2;
+                reduceCount++;
+                if (reduceCount === 6) {
+                    clearInterval(reduceImage);
+                }
+            }, 1000);
+        }
+    }, 1000);
 }
 
 // function getCookies() {
